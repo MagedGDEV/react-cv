3c532588918b38940e15e714ac042157398dd964
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-06-27 13:01:11 +0200

    remove unwanted files and create intial files

---
+++ src/components/Welcome/WelcomeImage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WelcomeImage(){
+
+}
+
+export default WelcomeImage;
---
+++ src/components/Welcome/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WelcomeMessage() {
+  
+}
+
+export default WelcomeMessage;
