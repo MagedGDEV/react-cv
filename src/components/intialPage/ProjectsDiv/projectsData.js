2fd4c7cb6c5bba1c4ae5b67df59994dd2969ef04
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-07-02 13:35:47 +0200

    fix typos in projectsData

--- src/components/intialPage/ProjectsDiv/projectsData.js
+++ src/components/intialPage/ProjectsDiv/projectsData.js
@@ -4,42 +4,42 @@ const projectsData = [
   {
     projectName: "Snake Game",
     projectDescription:
-      "Praticed the principles of OOP by implementing a snake game with some additional features added from monopoly.",
+      "Practiced the principles of OOP by implementing a snake game with some additional features added from monopoly.",
     logo: Logos.cPlusPlus,
     alt: "C++ Logo",
   },
   {
     projectName: "Restautrant Management",
     projectDescription:
-      "Practiced the principles of data structure and applied them on restaurant to manage its orders, customers, chefs, deliveries.",
+      "Practiced the principles of data structure and applied them to a restaurant to manage its orders, customers, chefs, and deliveries.",
     logo: Logos.cPlusPlus,
     alt: "C++ Logo",
   },
   {
     projectName: "Pong Game",
     projectDescription:
-      "Practiced the principles of assembly 8086 and understood the concepts of registers, stack and used it to create a 3 level pong game of different levels difficulties.",
+      "Practiced the principles of assembly 8086 and understood the concepts of registers, and stack, and used it to create a 3-level pong game of different levels difficulties.",
     logo: Logos.emu8086,
     alt: "Assembly logo",
   },
   {
     projectName: "Patient's Windows Form",
     projectDescription:
-      "Practiced the principles of SQL and relational database principles. Also, created a windows form and SQL Database to create an application to allow patients to communicate with doctors and pharmacies and allow doctor and pharmacies to communicate with their suppliers.",
+      "Practiced the principles of SQL and relational database principles. Also, created a windows form and SQL Database to create an application to allow patients to communicate with doctors and pharmacies and allow doctors and pharmacies to communicate with their suppliers.",
     logo: Logos.cSharp,
     alt: "C# logo",
   },
   {
     projectName: "Office Robot",
     projectDescription:
-      "Practiced the principles of microcontrollers, microprocessors and used arduino microcontroller to create a robot with seven functionalities for an office, Leaded a team formed of 23 members, set tasks and deadlines for the team.",
+      "Practiced the principles of microcontrollers and microprocessors and used Arduino microcontroller to create a robot with seven functionalities for an office. Leaded a team formed of 23 members and set tasks and deadlines for the team.",
     logo: Logos.arduino,
     alt: "Arduino logo",
   },
   {
     projectName: "Operating System Simulation",
     projectDescription:
-      "Practiced the principles of operating systems and how processes are orgnaized and placed in memory and created a program to simulate different algorithms used in linux.",
+      "Practiced the principles of operating systems and how processes are organized and placed in memory and created a program to simulate different algorithms used in Linux.",
     logo: Logos.c,
     alt: "C logo",
   },
@@ -47,28 +47,28 @@ const projectsData = [
   {
     projectName: "Office Reservation Application",
     projectDescription:
-      "Created an IOS application for IBM employees to reserve an office with specific data, room, time and room's size and their reasons and allow them to view their old reservations, whether they were accepted or not and ability to delete an old one.",
+      "Created an IOS application for IBM employees to reserve an office with specific data, room, time, and room size and their reasons and allow them to view their old reservations, whether they were accepted or not, and the ability to delete an old one.",
     logo: Logos.swift,
     alt: "Swift logo",
   },
   {
     projectName: "Twitter",
     projectDescription:
-      "Rebuilt twitter application for both IOS and Android by the help of flutter and dart, Leaded a team formed of 4 members, responsible for final product, distributing tasks using trello and interact with sub-team leaders, to coordinate the tasks and integrate and test the app.",
+      "Rebuilt Twitter application for both IOS and Android with the help of flutter and dart, Leaded a team formed of 4 members, responsible for the final product, distributing tasks using Trello and interact with sub-team leaders to coordinate the tasks and integrate and test the app.",
     logo: Logos.flutter,
     alt: "Flutter logo",
   },
   {
     projectName: "Search Engine",
     projectDescription:
-      "By the help of java implement webCrawler, indexer, ranker and store data into the database for the frontEnd developer to use and view appropriate links according to what is the user searching for.",
+      "With the help of java implement WebCrawler, indexer, ranker, and store data into the database for the frontEnd developer to use and view appropriate links according to what is the user searching for.",
     logo: Logos.java,
     alt: "Java logo",
   },
   {
-    projectName: "Automata Conversions ",
+    projectName: "Automata Conversions",
     projectDescription:
-      "Applied the conversions algorithms to transform regex into NFA, DFA and minDFA, produce JSON files which describe the finite automata and produce PNG photos of finite automata.",
+      "Applied the conversions algorithms to transform regex into NFA, DFA, and minDFA, produced JSON files that describe the finite automata, and generated PNG photos of finite automata.",
     logo: Logos.python,
     alt: "Python logo",
   },
